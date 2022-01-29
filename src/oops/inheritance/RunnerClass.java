4568469d63a58f9fc178ad0784ed9a7e4ee980ff
chithraup123 <chithra070315@gmail.com>
2022-01-29 12:55:46 +0530

    Remove whitespaces

--- src/oops/inheritance/RunnerClass.java
+++ src/oops/inheritance/RunnerClass.java
@@ -3,14 +3,13 @@ package oops.inheritance;
 public class RunnerClass {
 
 	public static void main(String[] args) {
-		
+
 		ChildClass child1 = new ChildClass("Mahesh", 25, "Actor");
 		child1.display();
-		
-		ChildClass child2  = (ChildClass) new Person("Alex", 20);
+
+		ChildClass child2 = (ChildClass) new Person("Alex", 20);
 		child2.display();
 
-		
 	}
 
 }
